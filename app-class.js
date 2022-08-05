4d79a46c89a9cb18f0c9eff373b1a6dfe454edb1
gfunk77 <faraci77@gmail.com>
2022-08-05 08:39:33 -0400

    class js

---
+++ app-class.js
@@ -0,0 +1,54 @@
+"use strict";
+
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    return element;
+  }
+  throw new Error(`Check ${selection} selector, no such element exists`);
+}
+
+class Counter {
+  constructor(element, value) {
+    this.counter = element;
+    this.value = value;
+    this.increaseBtn = element.querySelector(".increase");
+    this.decreaseBtn = element.querySelector(".decrease");
+    this.resetBtn = element.querySelector(".reset");
+    this.valueEl = element.querySelector(".value");
+    this.valueEl.textContent = this.value;
+    const increase = this.increase.bind(this);
+    const decrease = this.decrease.bind(this);
+    const reset = this.reset.bind(this);
+    this.increaseBtn.addEventListener("click", increase);
+    this.decreaseBtn.addEventListener("click", decrease);
+    this.resetBtn.addEventListener("click", reset);
+  }
+  increase() {
+    this.value++;
+    this.valueEl.textContent = this.value;
+    this.numberColor();
+  }
+  decrease() {
+    this.value--;
+    this.valueEl.textContent = this.value;
+    this.numberColor();
+  }
+  reset() {
+    this.value = 0;
+    this.valueEl.textContent = this.value;
+    this.numberColor();
+  }
+  numberColor() {
+    if (this.value > 0) {
+      this.valueEl.style.color = "green";
+    } else if (this.value < 0) {
+      this.valueEl.style.color = "red";
+    } else {
+      this.valueEl.style.color = "black";
+    }
+  }
+}
+
+const firstCounter = new Counter(getElement(".first-counter"), 0);
+const secondCounter = new Counter(getElement(".second-counter"), 0);
